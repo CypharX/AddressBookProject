63556a2154c92be33566cc72e68d23f64cd1aa02
Anthony D <CypharX@gmail.com>
2020-11-16 10:03:07 -0500

    trying things

---
+++ AddressBookProject/Data/Migrations/20201116144737_backToRegex.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AddressBookProject.Data.Migrations
+{
+    public partial class backToRegex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "AddressRecord",
+                maxLength: 14,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "AddressRecord",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 14);
+        }
+    }
+}
